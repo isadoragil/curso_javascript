24e3f1bf09707df1808cc477831e86dd5ecf57f4
Isadora Gil <105384553+isadoragil@users.noreply.github.com>
2024-02-20 13:28:22 -0300

    3.0

---
+++ ternario.js
@@ -0,0 +1,8 @@
+
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' //se for vdd retorna aprovado
+
+//expressão ? verdadeiro : falso
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
