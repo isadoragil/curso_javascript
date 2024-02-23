3ee7bbd489918af7d8640e2573b4dd1ef42b919d
Isadora Gil <105384553+isadoragil@users.noreply.github.com>
2024-02-23 13:32:00 -0300

    6.0

---
+++ 2. estrut_de_controle/while.js
@@ -0,0 +1,34 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima!')
+
+
+
+
+
+/*--------------------------------------------------------------------*/
+
+
+unction getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+} while (opcao != -1)
+
+console.log('Até a próxima!')
