ef345fb617de4d9818bc3358eea9d97ff5a6060b
Isadora Gil <105384553+isadoragil@users.noreply.github.com>
2024-02-29 17:31:12 -0300

    11.0

--- 3.função/arrowFunction.js
+++ 3.função/arrowFunction.js
@@ -16,3 +16,39 @@ let ola = function () {
 ola = () => 'Olá'
 ola = _ => 'Olá' // possui um param
 console.log(ola())
+
+
+
+/*------------------------------------------------------------*/
+
+function Pessoa() {
+    this.idade = 0
+
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+}
+
+new Pessoa
+
+/*------------------------------------------------------------*/
+
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
